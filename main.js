746468fe48b916eeeea460126ba70bba25cc559a
SaidamirRahimtorayev <saidamirxonrahimtorayev@gmail.com>
2024-04-16 14:53:07 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let name = prompt("Ismingizni kiriting...");
+let surname = prompt("Familiyangizni kiriting...");
+let age = prompt("Yoshingizni kiriting...");
+
+console.log(name);
+console.log(surname);
+console.log(age);
